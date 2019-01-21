615138e164116f8a15be0b7c0dbdf22dc84c8b24
Winston Chandra <winston.chandra@ui.ac.id>
2019-01-21 20:37:39 +0700

    changing some stuffs

--- src/App.js
+++ src/App.js
@@ -1,44 +1,80 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router, Route, Link } from 'react-router-dom';
+import React from "react";
+import { Router, Route, Link } from "react-router-dom";
 
-class App extends React.Component {
-   render() {
-      return (
-         <div>
-            <h1>{this.props.headerProp}</h1>
-			<Login/>
-		   <Router>
-		  <div>
-			<ul>
-			  <li><Link to="/">Home</Link></li>
-			</ul>
-			<hr/>
+function App() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
 
-			// All 3 components below would be rendered when in a homepage
-			<Route exact path="/" component={Home}/>
+        <hr />
 
-		  </div>
-		   </Router>
-         </div>
-      );
-   }
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/topics" component={Topics} />
+      </div>
+    </Router>
+  );
 }
 
-class Login extends React.Component {
-	render() {
-		return (
-			<h2>This is the login</h2>
-		);
-	}
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
 }
 
-class Home extends React.Component {
-	render() {
-		return (
-			<p>Home</p>
-		);
-	}
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Topics({ match }) {
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        <li>
+          <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+        </li>
+      </ul>
+
+      <Route path={`${match.path}/:topicId`} component={Topic} />
+      <Route
+        exact
+        path={match.path}
+        render={() => <h3>Please select a topic.</h3>}
+      />
+    </div>
+  );
+}
+
+function Topic({ match }) {
+  return (
+    <div>
+      <h3>{match.params.topicId}</h3>
+    </div>
+  );
 }
 
 export default App;
