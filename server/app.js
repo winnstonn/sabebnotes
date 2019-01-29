544294882b14849d8cc6a1677c61ea6435239ef7
Winston Chandra <winston.chandra@ui.ac.id>
2019-01-29 09:41:49 +0700

    adding cors dependency to resolved the problem of CORS policy

--- server/app.js
+++ server/app.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 8000;
 const bodyParser = require('body-parser');
 const authLogin = require('./routes/auth_login');
+// Set up a whitelist and check against it:
+var whitelist = ['http://example1.com', 'http://example2.com'] // Diisi sama url yang mau kita akses aja
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+// Then pass them to cors:
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 
