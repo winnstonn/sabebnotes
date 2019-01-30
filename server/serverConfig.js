1bc9df331c2a1ca928cd206d7594c8375100fce0
Winston Chandra <winston.chandra@ui.ac.id>
2019-01-30 20:57:01 +0700

    connecting data to DB

--- server/serverConfig.js
+++ server/serverConfig.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
     UNDEFINED: undefined,
-	DB_NAME: "",
-	DB_URL: ""
+	DB_NAME: "sabebnotes",
+	DB_URL: "mongodb://admin:adminoke1@ds217125.mlab.com:17125/sabebnotes"
 });
